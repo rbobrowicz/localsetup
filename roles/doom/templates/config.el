1cc04a0a593afac4d628cb1b9e8d758606c829ce
radek bobrowicz <radek@bobrowicz.me>
2020-05-12 14:46:13 -0500

    more cleanup

--- roles/doom/templates/config.el
+++ roles/doom/templates/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "radek bobrowicz"
-      user-mail-address "radek@bobrowicz.me")
+(setq user-full-name "{{ name }}"
+      user-mail-address "{{ email }}")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
